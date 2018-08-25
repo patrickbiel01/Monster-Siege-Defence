d33411624a3655bfab330124dd4d280b36f27fe6
patrickbiel01 <42307850+patrickbiel01@users.noreply.github.com>
2018-08-24 21:07:22 -0400

    Delete Zombie.swift

--- Biel_Patrick_Tower_Defence/Zombie.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Zombie.swift
-//  Biel_Patrick_Tower_Defence
-//
-//  Created by Period Three on 2018-05-11.
-//  Copyright © 2018 Period Three. All rights reserved.
-//
-
-import Foundation
