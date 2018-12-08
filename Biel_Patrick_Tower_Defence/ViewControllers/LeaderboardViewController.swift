11add8d0a6b416a5ee7e1c126189151ce0e35877
Patrick Biel <p.biel@outlook.com>
2018-12-07 22:49:27 -0500

    Leaderboard UI Improvements

--- Biel_Patrick_Tower_Defence/ViewControllers/LeaderboardViewController.swift
+++ Biel_Patrick_Tower_Defence/ViewControllers/LeaderboardViewController.swift
@@ -28,26 +28,32 @@ class LeaderboardViewController: UIViewController, UITableViewDataSource {
         //Retrieve object for persient data
         let userDefaults = UserDefaults.standard
         
+        //Clear UserDefaults: USED FOR TESTING
+//        let domain = Bundle.main.bundleIdentifier!
+//        UserDefaults.standard.removePersistentDomain(forName: domain)
+//        UserDefaults.standard.synchronize()
+        
         //Retrieve high scores
         guard let highScoreNames = userDefaults.object(forKey: LeaderboardViewController.NAME_KEY) as? Array<String> else {
-            self.highScoreNames = ["Empty"]
+            self.highScoreNames = ["Empty","Empty","Empty","Empty","Empty","Empty"]
+            self.highScoreValues = [0,0,0,0,0,0]
             return
         }
         //Retrieve high score names
         guard let highScoreValues = userDefaults.object(forKey: LeaderboardViewController.SCORE_KEY) as? Array<Int> else {
+            self.highScoreNames = ["Empty"]
             self.highScoreValues = [0]
             return
         }
         
-        //Assign retrieved valiues to member arrays
+        //Assign retrieved values to member arrays
         self.highScoreNames = highScoreNames
         self.highScoreValues = highScoreValues
-        
     }
     
     /* Function called when Table View is loaded */
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //Create n tables
+        //Create n cells
         return highScoreValues.count
     }
     
@@ -55,9 +61,18 @@ class LeaderboardViewController: UIViewController, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //Create new cell
         let myCell = UITableViewCell()
-        //Variable to store cell text
-        //Apply text
+        
+        /* Set styles for tableview */
+        //Background
+        tableView.backgroundColor = .clear
+        myCell.backgroundColor = .clear
+        tableView.tableFooterView = UIView()
+        //Text colour
+        myCell.textLabel!.textColor = .white
+        
+        //Set text
         myCell.textLabel?.text = "\(indexPath.row + 1). \t\t" + highScoreNames[indexPath.row] + ": " + String(highScoreValues[indexPath.row])
+        
         //Apply cell
         return myCell
     }
