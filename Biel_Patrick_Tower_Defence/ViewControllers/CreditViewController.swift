a623cd5087089efa5ea2fc431407de8d5e1da440
Patrick Biel <p.biel@outlook.com>
2018-12-10 18:27:45 -0500

    Credit

---
+++ Biel_Patrick_Tower_Defence/ViewControllers/CreditViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CreditViewController.swift
+//  Biel_Patrick_Tower_Defence
+//
+//  Created by Patrick Biel on 2018-12-10.
+//  Copyright © 2018 Patrick Biel. All rights reserved.
+//
+
+import UIKit
+
+class CreditViewController: UIViewController {
+
+    @IBAction func dismissClicked(_ sender: Any) {
+        //Exit
+        dismiss(animated: true, completion: nil)
+    }
+
+}
